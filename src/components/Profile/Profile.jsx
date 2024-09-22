23020b54f7b7e145e1683bc99b189e34c60e9cfc
Bohdan Haponiuk <jlomka54@gmail.com>
2024-09-22 12:31:36 +0400

    add normalize

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,33 @@
 import style from "./Profile.module.css"
 
 const Profile = ({ name, tag, location, image, stats:{followers, views, likes} }) => {
-    return <div>
-<div className={style.image}>
-  <img
-    src={image}
- alt="User avatar"
- />
- <p>{name}</p>   
- <p>{tag}</p>
-            <p>{location}</p>
- </div>
+  return (<div>
+    <div className={style.image}>
+      <img
+        src={image}
+        alt="User avatar"
+      />
+      <p>{name}</p>
+      <p>{tag}</p>
+      <p>{location}</p>
+    </div>
 
- <ul>
- <li>
- <span>Followers</span>
- <span>{followers}</span>
- </li>
- <li>
-<span>Views</span>
- <span>{views}</span>
- </li>
- <li>
- <span>Likes</span>
- <span>{likes}</span>
-</li>
- </ul>
-</div>
-
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>{followers}</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>{views}</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>{likes}</span>
+      </li>
+    </ul>
+  </div>
+  );
  };
 
 
