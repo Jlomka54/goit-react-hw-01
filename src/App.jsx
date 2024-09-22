49b8c27aa45666c59f21abcbeff00944f8bc298f
Bohdan Haponiuk <jlomka54@gmail.com>
2024-09-22 12:23:47 +0400

    add profile

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from "./components/Profile/Profile.jsx";
+import userData from "./userData.json"
+
+const App = () => {
+    <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+
+    />
+
+}
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import style from "./Profile.module.css"
+
+const Profile = ({ name, tag, location, image, stats:{followers, views, likes} }) => {
+    return <div>
+<div className={style.image}>
+  <img
+    src={image}
+ alt="User avatar"
+ />
+ <p>{name}</p>   
+ <p>{tag}</p>
+            <p>{location}</p>
+ </div>
+
+ <ul>
+ <li>
+ <span>Followers</span>
+ <span>{followers}</span>
+ </li>
+ <li>
+<span>Views</span>
+ <span>{views}</span>
+ </li>
+ <li>
+ <span>Likes</span>
+ <span>{likes}</span>
+</li>
+ </ul>
+</div>
+
+ };
+
+
+export default Profile;
